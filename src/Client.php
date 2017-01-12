85abf95773a80f87ba33e49cb639608c6189d450
Nathan Oertel <nathan@digitalclouddesigns.com>
2017-01-11 22:36:59 -0500

    First Commit

---
+++ src/Client.php
@@ -0,0 +1,25 @@
+<?php
+namespace MagentoV1API;
+
+class Client {
+	
+	private $client;
+	
+	private $session;
+	
+	public function __construct($config) {
+		if(empty($config['baseUrl']) || empty($config['username']) || empty($config['apiKey'])) {
+			throw new \Exception('Client Not Configured');
+		} else {
+			echo $config['baseUrl'].'/api/soap/?wsdl'."\n";
+			$this->client = new \SoapClient($config['baseUrl'].'/api/soap/?wsdl');
+			
+			
+			$this->session = $this->client->login($config['username'], $config['apiKey']);
+		}
+	}
+	
+	public function call($method, $args = null) {
+		return  $this->client->call($this->session, $method, $args);
+	}
+}
